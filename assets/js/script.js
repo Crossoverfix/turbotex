b04d2e85b04a3b688c8b1b10e2132d914ceb3ed2
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2019-05-11 20:37:35 +0300

    v1.05

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
-
+    var $formPrev = $('.check-form__wrap__control__prev');
+    var $formNext = $('.check-form__wrap__control__next');
+    var $formCard = $('.check-form__wrap__content__card');
+    $formNext.on('click',function () {
+        pages('next');
+        return false;
+    })
+    $formPrev.on('click',function () {
+        pages('prev');
+        return false;
+    })
+    $formCard.on('click',function () {
+        $(this).toggleClass('active');
+        return false;
+    })
+    function pages($direction) {
+        let $temp = 0;
+        let $tempContent = $('.check-form__wrap__content');
+        for (i = 0 ; i < $tempContent.length; i++){
+            if($tempContent.eq(i).hasClass('active')){
+                $temp = i;
+            }
+        }
+        if($direction == 'next'){
+            if($temp >= $tempContent.length - 1){
+                $tempContent.removeClass('active');
+                $tempContent.eq(0).addClass('active');
+            } else {
+                $tempContent.removeClass('active');
+                $tempContent.eq($temp + 1).addClass('active');
+            }
+        } else if ($direction == 'prev'){
+            if($temp <= 0){
+                $tempContent.removeClass('active');
+                $tempContent.eq($tempContent.length - 1).addClass('active');
+            } else {
+                $tempContent.removeClass('active');
+                $tempContent.eq($temp - 1).addClass('active');
+            }
+        }
+    }
 })
