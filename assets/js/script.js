89ead7dc03b8d8088ab86f6c2b2eb2c3616bb89a
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2019-05-12 13:40:51 +0300

    v1.06

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,4 +40,10 @@ $(document).ready(function () {
             }
         }
     }
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map("ymaps", {
+            center: [55.76, 37.64],
+            zoom: 10
+        });
+    });
 })
